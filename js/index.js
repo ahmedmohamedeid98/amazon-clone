ee2e473d283973a50b822a694df27fbd81b632d5
ahmedmohamedeid98 <ahmedmohamedeid98@gmail.com>
2021-12-01 16:03:33 +0200

    update index.js to use jQuery instead of DOM

--- js/index.js
+++ js/index.js
@@ -1,35 +1,39 @@
-// let a_signin = document.getElementById("sing_in_a_id");
-let signin_or_username = document.getElementById("sign_in_or_username");
-let sign_in_out_button = document.getElementById('sign_in_out');
+$(document).ready(() => {
+  // get ui items
+  const signinLabel = $('#sign_in_or_username');
+  const signinBtn = $('#sign_in_out');
+  const registerItem = $('#register_nav_item');
 
+  // check if user logged in or not
+  var isLoggedIn = localStorage.isLoggedIn ? true : false;
 
-var isLoggedIn = localStorage.isLoggedIn ? true : false;
-
-
-if (isLoggedIn) {
-  let data = localStorage.getItem("currentUser");
-  if (data) {
-    let user = JSON.parse(data);
-    sign_in_out_button.textContent = "Sing out";
-    signin_or_username.textContent = user.username;
-  }
-} else {
-  sign_in_out_button.textContent = "Signin";
-  signin_or_username.textContent = "sign in"
-}
-
-
-sign_in_out_button.addEventListener("click", () => {
-  if(isLoggedIn) {
-    localStorage.removeItem("isLoggedIn");
-    localStorage.removeItem("currentUser");
+  // init data
+  if (isLoggedIn) {
+    // get current logged user if exist
+    let data = localStorage.getItem("currentUser");
+    if (data) {
+      let user = JSON.parse(data);
+      registerItem.hide();
+      signinBtn.text("Sign out");
+      signinLabel.text(user.username);
+    }
+  } else {
+    registerItem.show();
+    signinBtn.text("Signin");
+    signinLabel.text("sign in");
   }
 
-  var url = location.origin + location.pathname.replace("index.html", "pages/login.html");
-  location.replace(url);
-  // window.location.replace("C:/Users/ahmed/OneDrive/Documents/0. HTML&CSS/project/amazon-clone/pages/login.html");
-  // window.location.href = '../pages/login.html';
-});
+  // handel click event on signin/signout button
+  signinBtn.on("click", () => {
+    // if user logged in clear user data
+    if (isLoggedIn) {
+      localStorage.removeItem("isLoggedIn");
+      localStorage.removeItem("currentUser");
+    }
+    // redired user to login page
+    var url = location.origin + location.pathname.replace("index.html", "pages/login.html");
+    location.replace(url);
+  });
 
 
 
@@ -41,42 +45,40 @@ sign_in_out_button.addEventListener("click", () => {
 
 
 
+  //=============================
+  //      SlideShow
+  //=============================
 
+  const prev = $(".prev");
+  const next = $(".next");
+  const slides = $(".mySlides");
+  var n = slides.length;
+  var index = 0;
+  var prevIndex = 0;
 
-
-
-
-
-//=============================
-//      SlideShow
-//=============================
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) {
-    slideIndex = 1
-  }
-  if (n < 1) {
-    slideIndex = slides.length
-  }
-  for (i = 0; i < slides.length; i++) {
+  // init show at index 0 and hide the remaining
+  slides[0].style.display = "block";
+  for (let i = 1; i < n; i++)
     slides[i].style.display = "none";
+  
+
+  prev.on("click", () => {
+    // index--; if (index < 0) index = n - 1 
+    prevIndex = index;
+    index = (index + n - 1) % n;
+    showCurrentAndHidePrevIndex();
+  });
+
+  next.on("click", () => {
+    // index++; if (index >= n) index = 0
+    prevIndex = index;
+    index = (index + 1) % n;
+    showCurrentAndHidePrevIndex();
+  });
+
+  function showCurrentAndHidePrevIndex() {
+    slides[index].style.display = "block";
+    slides[prevIndex].style.display = "none";
   }
-  // for (i = 0; i < dots.length; i++) {
-  //   dots[i].className = dots[i].className.replace(" active", "");
-  // }
-  slides[slideIndex - 1].style.display = "block";
-  // dots[slideIndex - 1].className += " active";
-}
+
+});
