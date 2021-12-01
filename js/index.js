eb9a40f4711490c9fd83b89628f4ad0fd199231d
ahmedmohamedeid98 <ahmedmohamedeid98@gmail.com>
2021-12-01 21:04:52 +0200

    add ability to signout in nav bar

--- js/index.js
+++ js/index.js
@@ -1,39 +1,5 @@
 $(document).ready(() => {
-  // get ui items
-  const signinLabel = $('#sign_in_or_username');
-  const signinBtn = $('#sign_in_out');
-  const registerItem = $('#register_nav_item');
-
-  // check if user logged in or not
-  var isLoggedIn = localStorage.isLoggedIn ? true : false;
-
-  // init data
-  if (isLoggedIn) {
-    // get current logged user if exist
-    let data = localStorage.getItem("currentUser");
-    if (data) {
-      let user = JSON.parse(data);
-      registerItem.hide();
-      signinBtn.text("Sign out");
-      signinLabel.text(user.username);
-    }
-  } else {
-    registerItem.show();
-    signinBtn.text("Signin");
-    signinLabel.text("sign in");
-  }
-
-  // handel click event on signin/signout button
-  signinBtn.on("click", () => {
-    // if user logged in clear user data
-    if (isLoggedIn) {
-      localStorage.removeItem("isLoggedIn");
-      localStorage.removeItem("currentUser");
-    }
-    // redired user to login page
-    var url = location.origin + location.pathname.replace("index.html", "pages/login.html");
-    location.replace(url);
-  });
+  
 
 
 
--- js/nav_down_menu.js
+++ js/nav_down_menu.js
@@ -1,18 +1,51 @@
-let signInBlock = document.getElementById('sing_in_a_id');
-let menu = document.getElementById('menu');
-let signInOrSignOut = document.getElementById('');
+$(document).ready(() => {
+    // get ui items
+    const signinLabel = $('#sign_in_or_username');
+    const signinBtn = $('#sign_in_out');
+    const registerItem = $('#register_nav_item');
+    const signInBlock = $('#sing_in_a_id');
+    const menu = $('#menu')
 
-
-signInBlock.addEventListener('mouseover', () => {
-    menu.classList.add('active')
-})
-
-
-menu.addEventListener('mouseleave', () => {
-    menu.classList.remove('active')
-})
-
-signInBlock.addEventListener('click', e => {
+    signInBlock.on("mouseover click", (e) => {
     e.preventDefault()
-    menu.classList.add('active')
-})
+        menu.addClass('active')
+    })
+
+    menu.on('mouseleave', () => {
+        menu.removeClass('active')
+    })
+
+
+    
+
+    // check if user logged in or not
+    var isLoggedIn = localStorage.isLoggedIn ? true : false;
+
+    // init data
+    if (isLoggedIn) {
+        // get current logged user if exist
+        let data = localStorage.getItem("currentUser");
+        if (data) {
+            let user = JSON.parse(data);
+            registerItem.hide();
+            signinBtn.text("Sign out");
+            signinLabel.text(user.username);
+        }
+    } else {
+        registerItem.show();
+        signinBtn.text("Signin");
+        signinLabel.text("sign in");
+    }
+
+    // handel click event on signin/signout button
+    signinBtn.on("click", () => {
+        // if user logged in clear user data
+        if (isLoggedIn) {
+            localStorage.removeItem("isLoggedIn");
+            localStorage.removeItem("currentUser");
+        }
+        // redired user to login page
+        var url = location.origin + location.pathname.replace("index.html", "pages/login.html");
+        location.replace(url);
+    });
+});
